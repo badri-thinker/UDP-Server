d34a5f3a839905a44e27430ba83136afe1026ed0
badri-thinker <42091963+badri-thinker@users.noreply.github.com>
2018-09-26 10:07:31 -0400

    Create server.py

---
+++ server.py
@@ -0,0 +1,19 @@
+import os
+    try:
+        rsd=mysoc.socket(mysoc.AF_INET, mysoc.SOCK_DGRAM)
+        
+    except mysoc.error as err:
+        exit()
+
+# Define the port on which you want to receive  from  the server
+    Rport = 50007
+    myip = mysoc.gethostbyname(mysoc.gethostname())
+# connect to the server on local machine
+    server_binding=(myip, Rport)
+    rsd.bind(server_binding)
+    data,addr=rsd.recvfrom(1024)
+    print(data.decode("utf-8"))
+
+# Close the  receiver socket 
+ rsd.close()
+exit()
